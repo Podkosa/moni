2b1630aef4b4d190e84ad5848dcbda03dc055508
Podkosa <artem_podkolzin@inbox.ru>
2022-10-28 20:10:31 +0300

    tweak loading checkers

--- src/checkers/__init__.py
+++ src/checkers/__init__.py
@@ -1,5 +1,5 @@
 from .flower import FlowerChecker
-from .utils import get_all_checkers, check_all, monitor
+from .utils import get_loaded_checkers, check_all, monitor
 from .abstract import CheckerError
 
 __loaded_checkers__ = [] # keep a cached list of all the Checkers parsed from settings
--- src/checkers/utils.py
+++ src/checkers/utils.py
@@ -10,7 +10,8 @@ def get_checker_cls(checker_name: str) -> type[Checker]:
         raise ValueError(f'Checker {checker_name} is not defined in settings')
     return getattr(checkers, f'{checker_name.capitalize()}Checker')
 
-def get_all_checkers() -> list[Checker]:
+def get_loaded_checkers() -> tuple[Checker]:
+    """Loads, caches and returns checker instances."""
     if not checkers.__loaded_checkers__:
         for checker_name, conf in settings.CHECKERS.items():
             Checker_cls = get_checker_cls(checker_name)
@@ -19,16 +20,16 @@ def get_all_checkers() -> list[Checker]:
                 params['handlers'] = [handlers.get_handler_cls(handler_name)() for handler_name in handler_names]
                 checker = Checker_cls(host=server, **params)
                 checkers.__loaded_checkers__.append(checker)
-    return checkers.__loaded_checkers__
+    return tuple(checkers.__loaded_checkers__)
 
 async def check_all():
     """Run all checks from settings.CHECKERS. Alert through handlers."""
     async with asyncio.TaskGroup() as tg:
-        for checker in get_all_checkers():
+        for checker in get_loaded_checkers():
             tg.create_task(checker.run())
 
 async def monitor():
     """Start full monitoring"""
     async with asyncio.TaskGroup() as tg:
-        for checker in get_all_checkers():
+        for checker in get_loaded_checkers():
             tg.create_task(checker.monitor())
